6501009732e085d621de8c754846f48e7ed315ca
Abhishek Bhattacharjee <127030695+Abhishek-465@users.noreply.github.com>
2024-02-06 01:08:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
