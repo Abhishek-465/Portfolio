548ccff2dbb0ba9167fe051e937eda22d1262fb7
Abhishek-465 <abhishekbhattacharjee12345@gmail.com>
2024-03-15 23:06:22 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 1000); // Fade-out animation duration
+  }, 1000); // Loader display duration
+});
+
 function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
